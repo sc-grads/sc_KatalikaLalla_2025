893d8db0a979b47f9c653a04a14402ebb5eebcdf
Katalika Lalla <katalika.lalla@sambeconsulting.com>
2025-04-24 10:06:28 +0200

    First table creation with personal info, such as firstname, surname, dob and ID.

---
+++ DatabaseAdministration/SQLStatements/Udemy/InsertIntoTable1.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO [dbo].[personalInfo]
+           ([firstName]
+           ,[lastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Katalika'
+		   ,'Lalla'
+		   ,'05/18/2001'
+		   ,123)
+GO
+
+select * from [dbo].[personalInfo]
+
+GO
+
