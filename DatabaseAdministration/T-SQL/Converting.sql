593c44fe9a723e079b2184030db10a79fcca1673
Katalika Lalla <katalika.lalla@sambeconsulting.com>
2025-05-21 07:27:16 +0200

    Implicit and explicit data type conversion.

---
+++ DatabaseAdministration/T-SQL/Converting.sql
@@ -0,0 +1,16 @@
+-- IMPLICIT
+
+DECLARE @myvar as Decimal(5,2) = 3
+
+SELECT @myvar
+
+-- explicit
+
+SELECT CONVERT(decimal(5,2),3)/2
+SELECT CAST(3 as decimal(5,2))/2
+
+SELECT 3/2 -- EQUALS 1
+SELECT 3/2.0 -- EQUALS 1.5
+
+SELECT CONVERT(INT,12.345)+CONVERT(INT,12.7) -- This equals 24.
+SELECT CONVERT(INT,12.345+12.7) -- This equals 25.
